29a148713fdc87cd4f4e3f40fc13d26221ebb647
Kennedy Sutton <kjsutton8@gmail.com>
2024-04-30 04:47:45 -0400

    commit

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--1(yv^58ms3-&i&+9$!*e_3t(y(iwbx1k6&86c4_jqrtfbg2o)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
